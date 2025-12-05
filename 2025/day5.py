8803c88adc5eee51dc14ad258d36736fa5eea7b3
Roberts, Paul E 1LT USARMY SOFTWARE FACTORY (USA) <paul.e.roberts117.mil@army.mil>
2025-12-05 17:06:42 -0600

    yep

--- 2025/day5.py
+++ 2025/day5.py
@@ -1,3 +1,4 @@
+import operator
 def day5():
     with open("inputs/day5.txt") as f:
         list = f.read().split("\n")
@@ -25,19 +26,30 @@ def day5():
 def day5pt2():
     with open("inputs/day5.txt") as f:
         list = f.read().split("\n")
-        keys = [c.split("-") for c in list]
+        keys = (c.split("-") for c in list)
         resultKeys = []
         flip = False
         for inner_list in keys:
             if inner_list[0] != "" and not flip:
-                resultKeys.append(inner_list)
+                resultKeys.append((int(inner_list[0]), int(inner_list[1])))
             else:
                 flip = True
                 break
 
         score = 0
+        newRanges = []
+        resultKeys.sort(key= lambda item: item[0])
+        newRanges.append(resultKeys[0])
         for key in resultKeys:
-            score += int(key[0]) + int(key[1])
+            for addedKey in newRanges:
+                if key[0] > addedKey[1]:
+                    newRanges.append(key)
+                if key[0] < addedKey[0]:
+                    if key[1] > addedKey[1]:
+                        newRanges.append(key)
+
+        for newKeys in newRanges:
+            score += newKeys[0] + newKeys[1]
         # score += 1 if resultKeys[n+1][0] > value > resultKeys[n][1] for value in resultValues
         print(score)
 
