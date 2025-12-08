d8bde3df859b277d3846804b08639f53223e2f37
Roberts, Paul E 1LT USARMY SOFTWARE FACTORY (USA) <paul.e.roberts117.mil@army.mil>
2025-12-08 08:20:59 -0600

    yep

--- 2025/day6.py
+++ 2025/day6.py
@@ -22,30 +22,9 @@ def day6pt1():
         print(final)
 
 def day6pt2():
-    with open("inputs/day6.txt") as f:
+    with open("inputs/day6test.txt") as f:
         lines = f.read().split("\n")
-        *number_lines, op_line = lines
-        width = max(len(line) for line in lines)
-        number_lines = [line.ljust(width) for line in number_lines]
-        print(number_lines)
-        op_line = op_line.ljust(width)
-        op_positions = [i for i, c in enumerate(op_line) if c in '*+']
-        total = 0
-        prev_pos = -1
-        for op_pos in op_positions:
-            op = op_line[op_pos]
-            numbers = []
-            for row in number_lines:
-                segment = row[prev_pos + 1 : op_pos + 1]
-                digits = [c for c in segment if c.isdigit()]
-                if digits:
-                    numbers.append(int(''.join(reversed(digits))))
-            result = numbers[0]
-            for n in numbers[1:]:
-                result = result * n if op == '*' else result + n
-            total += result
-            prev_pos = op_pos
-        print(total)
+
 
 if __name__ == "__main__":
-    day6pt2()
+    day6pt1()
